2b537a135dc0c2c6049cb2a34f044027c5a3c5fc
nacxa <67007250+NACXA0@users.noreply.github.com>
2023-07-20 09:19:18 +0800

    GLM-API.py

--- logic/GLM-API.py
+++ logic/GLM-API.py
@@ -1,5 +1,5 @@
 #安装环境包pip install zhipuai
-'''APIKey：03b60d7b276a44f7a33619aa57fe60f9'''
+'''APIKey：'''
 '''三种API调用方式
 同步调用：zhipuai.model_api.invoke()调用后即可一次性获得最终结果
 异步调用：zhipuai.model_api.async_invoke()调用后会立即返回一个任务 ID ，然后用任务ID查询调用结果（根据模型和参数的不同，通常需要等待10-30秒才能得到最终结果
